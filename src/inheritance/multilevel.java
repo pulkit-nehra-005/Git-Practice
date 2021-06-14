acbabe3e972f02d1e4ae40e67252d4fd75b68344
pulkit-nehra-005 <1702780.cse.cec@cgc.edu.in>
2021-06-14 17:31:42 +0530

    making changes in file

--- src/inheritance/multilevel.java
+++ src/inheritance/multilevel.java
@@ -4,5 +4,12 @@ class multilevel{
 
     public static void main(String args[]){
         System.out.println("Hello World");
+        multilevel obj = new multilevel();
+        System.out.println(obj.func());
+    }
+
+
+    int func(){
+        return 2;
     }
 }
